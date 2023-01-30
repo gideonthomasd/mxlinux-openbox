a7fbde7aa2d9d7c4596c0f2b093d4d32e01df8f1
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2023-01-30 16:15:13 +0000

    Add files via upload

---
+++ openbox/kill.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sleep 15
+killall xdg-desktop-portal-gtk
+killall xfce4-notifyd
+killall xdg-desktop-portal
+killall xdg-document-portal
+killall xdg-permission-store
+killall dconf-service
+
+
+
+

98abfcf6b69b1a4d0f922a349ddde92974e9bbdf
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2023-02-27 18:31:01 +0000

    Create 130_pacstall.sh

---
+++ 130_pacstall.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+
+sudo bash -c "$(curl -fsSL https://git.io/JsADh || wget -q https://git.io/JsADh -O -)"
+
+pacstall -I balena-etcher-deb
+pacstall -I pycharm-community-bin
+pacstall -I nerd-fonts-jetbrains-mono
+#pacstall -I zoom-deb

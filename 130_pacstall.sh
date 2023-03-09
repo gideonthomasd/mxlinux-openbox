bcab62e7c3536e090d2d8c9712069ed9ecbac8c7
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2023-03-09 19:31:49 +0000

    Update 130_pacstall.sh

--- 130_pacstall.sh
+++ 130_pacstall.sh
@@ -6,3 +6,5 @@ pacstall -I balena-etcher-deb
 pacstall -I pycharm-community-bin
 pacstall -I nerd-fonts-jetbrains-mono
 #pacstall -I zoom-deb
+
+sudo apt install python3-tk python3-pil python3-pil.imagetk sqlitebrowser python3-pip
